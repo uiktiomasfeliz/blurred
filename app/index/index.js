85cb1f6d47c62e3d49db65fc6e6c3fa34dd5682e
uiktiomasfeliz <uiktiomasfeliz@gmail.com>
2015-09-11 16:59:21 +0200

    ocLazyLoad, angular-facebook ADDED

--- app/index/index.js
+++ app/index/index.js
@@ -1,12 +1,27 @@
 'use strict';
 
-angular.module('myApp.index', ['ngRoute', 'swipe', 'snapscroll'])
+angular.module('myApp.index', ['ngRoute', 'swipe', 'snapscroll', 'oc.lazyLoad', 'facebook'])
 
-.config(['$routeProvider', function($routeProvider) {
+.config(['$routeProvider', '$ocLazyLoadProvider', 'FacebookProvider', function($routeProvider, $ocLazyLoadProvider, FacebookProvider) {
   $routeProvider.when('/', {
     templateUrl: 'index/index.html',
-    controller: 'Index1Ctrl'
+    controller: 'Index1Ctrl'/*,
+    resolve: {
+      loadMyPlugins: ['$ocLazyLoad', function($ocLazyLoad) {
+      // you can lazy load files for an existing module
+             return $ocLazyLoad.load([
+                {
+                  name: 'facebook',
+                  files: ['bower_components/angular-facebook/lib/angular-facebook.js']
+                }
+             ]);
+        }
+      ]
+    }*/
   });
+
+  FacebookProvider.init('1574520766164734');
+
 }])
 //snapscroll directive for keyboards!
 .directive('keyboardKeys', ['$document', function ($document) {
@@ -30,7 +45,7 @@ angular.module('myApp.index', ['ngRoute', 'swipe', 'snapscroll'])
     }
   }
 }])
-.controller('Index1Ctrl', ['$scope','$window',function($scope, $window) {
+.controller('Index1Ctrl', ['$scope', '$window', 'Facebook', function($scope, $window, Facebook) {
     //snapscroll settings
     var index = parseInt($window.location.hash.slice(1), 10);
     $scope.snapAnimation = false; // turn animation off for the initial snap on page load
@@ -59,5 +74,30 @@ angular.module('myApp.index', ['ngRoute', 'swipe', 'snapscroll'])
       $scope.snapAnimation = true; // turn animations on after the initial snap
       //$window.location.hash = snapIndex;
     };
+
+    //facebook
+    $scope.login = function() {
+      // From now on you can use the Facebook service just as Facebook api says
+      Facebook.login(function(response) {
+        // Do something with response.
+      });
+    };
+
+    $scope.getLoginStatus = function() {
+      Facebook.getLoginStatus(function(response) {
+        if(response.status === 'connected') {
+          $scope.loggedIn = true;
+        } else {
+          $scope.loggedIn = false;
+        }
+      });
+    };
+
+    $scope.me = function() {
+      Facebook.api('/me', function(response) {
+        $scope.user = response;
+      });
+    };
+
 }])
 ;
